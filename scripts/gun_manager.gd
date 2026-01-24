29d615f438ee554413a270ae822c45320f19c11e
asdwhalo <unuvarhalil01@gmail.com>
2026-01-24 13:38:04 +0300

    weapon system is bit bugged but works(itink)

--- scripts/gun_manager.gd
+++ scripts/gun_manager.gd
@@ -10,7 +10,8 @@ enum weapon_state {
 @export var gun_array:Array[Weapon]
 @export var current_weapon:Weapon
 @onready var shoot_point: Node3D = %shoot_point
-@export var anim: AnimationPlayer
+@export var anim:AnimationPlayer
+#@onready var anim: AnimationPlayer = current_weapon.ins_scene.get_node("basicPistol/AnimationPlayer")
 @export var parent : Player 
 func _ready() -> void:
 	initilize_weapon()
@@ -29,10 +30,10 @@ func initilize_weapon() -> void:
 func fire():
 	var bullet = bullet_scene.instantiate()
 	if Input.is_action_pressed("fire") and parent.is_cap:
-		if !anim.is_playing():
+		if  anim == null  or (!anim.is_playing() or anim.has_animation("shoot") and not anim.is_playing()):
 			anim.play("shoot") # animasyon bulunamıyor 
 			add_child(bullet)
 			bullet.global_transform.basis = shoot_point.global_transform.basis
 			bullet.global_position = shoot_point.global_position
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	fire()
--- scripts/resources/weapon.gd
+++ scripts/resources/weapon.gd
@@ -4,4 +4,9 @@ extends Resource
 @export var damage:float = 1.0
 @export var scene:PackedScene = null
 @export var shoot_position:Vector3
+#var anim = scene.get_node("AnimationPlayer")
+var ins_scene:Node
 #@export var anim:AnimationPlayer #resourceler bunu tutamıyor
+func _init() -> void:
+	#scene = ins_scene.instantiate()
+	pass
